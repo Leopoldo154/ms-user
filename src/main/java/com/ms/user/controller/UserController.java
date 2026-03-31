83cefe53c88a95c0450364014d28b3a6561769ae
Leopoldo154 <80859477+Leopoldo154@users.noreply.github.com>
2026-03-31 18:17:50 -0300

    Add save user

---
+++ src/main/java/com/ms/user/controller/UserController.java
@@ -0,0 +1,28 @@
+package com.ms.user.controller;
+
+import com.ms.user.dto.UserRecordDto;
+import com.ms.user.models.UserModel;
+import com.ms.user.service.UserService;
+import jakarta.validation.Valid;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/users")
+    public ResponseEntity<UserModel> saveUser(@RequestBody @Valid UserRecordDto userDto){
+        var userModel = new UserModel();
+        BeanUtils.copyProperties(userDto, userModel);
+        return ResponseEntity.status(HttpStatus.CREATED).body(userService.save(userModel));
+    }
+
+}
